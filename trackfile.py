f77175f182d63132f833f3c0e462b3dd469e54d6
Sanjay Nandy <sanjaynandy89@gmail.com>
2020-04-06 01:47:52 -0700

    Read and write function added

--- trackfile.py
+++ trackfile.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 #create phonebook.dat file
 #open file in wb mode as binary file
-with open("phonebook.dat","wb") as f:
+import mmap,sys
+def WriteBinary():
+    with open("phonebook.dat","wb") as f:
     #write data for the file
-    n=int(input('How many entries ?'))
-    for i in range(n):
+       n=int(input('How many entries ?'))
+       for i in range(n):
           name=input('Enter name')
           phone=input('Enter  phone no')
-    name=name.encode()
-    phone=phone.encode()
-    #write data into the file
-    f.write(name+phone)
+          name=name.encode()
+          phone=phone.encode()
+          #write data into the file
+          f.write(name+phone)
+    return
+def ReadBinary():
+   with open("phonebook.dat","r+b") as f:
+     mm=mmap.mmap(f.fileno(),0)
+     print(mm.read().decode())
+   return
+print("Enter 1 for write 2 for read")
+ch=input("Enter the choice")
+if(ch=='1'):
+   WriteBinary()
+if(ch=='2'):
+   ReadBinary()
