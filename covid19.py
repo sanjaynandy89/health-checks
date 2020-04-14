ddb28c839c0f6919d7427cbafd09f5d45b0c1d6f
sanjaynandy89 <sanjaynandy89@gmail.com>
2020-04-14 01:33:47 -0400

    Create covid19.py
    Clustering covid19 affected area according Who guidelines.

---
+++ covid19.py
@@ -0,0 +1,11 @@
+def WhoGuidLines(affected):
+     if affected<1000:
+         print("Blue Zone Cluster")
+     elif affected<10000:
+         print("Green Zone Cluster")
+     elif affected<50000:
+         print("Yellow Zone Cluster")
+     else:
+          print("Red Zone Cluster")
+     
+
