c8b543b4f5845672e4d1912c3375d60d7058afe3
Sanjay Nandy <sanjaynandy89@gmail.com>
2020-10-27 11:19:02 -0400

    added another zone

--- covid19.py
+++ covid19.py
@@ -5,7 +5,9 @@ def WhoGuidLines(affected):
          print("Green Zone Cluster")
      elif affected<50000:
          print("Yellow Zone Cluster")
-     else:
-          print("Red Zone Cluster")
+     elif: affected<25000:
+         print("Pink Zone Cluster")
+	 else:
+	     print("Red Zone Cluster")
      
 
